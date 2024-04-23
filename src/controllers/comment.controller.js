cf9fa9276d2e6b009935b691e3bc20f74f30ad63
Vishesh <vishesh.gubrani@gmail.com>
2024-04-23 19:56:55 +0530

    Added Controllers

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,20 +3,86 @@ import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
+    if (!videoId) {
+        throw new ApiError(400, "Video Not Found")
+    }
 
+    const options = {
+        page: parseInt(page, 10) || 1,
+        limit: parseInt(limit, 10) || 10,
+        populate: 'owner'
+    }
+
+    const results = await Comment.aggregatePaginate(
+        {video: videoId},
+        options
+    )
+
+    return res.status(200).json(
+        new ApiResponse(200, results, "Comments Fetched Successfully")
+    )
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {content} = req.body
+    const {videoId} = req.params
+
+    const video = await Video.findById(videoId)
+
+    if (!video) {
+        throw new ApiError(400, "Video could not be found")
+    }
+
+ 
+    if (!content) {
+        throw new ApiError(400, "Please Enter the comment")
+    }
+    
+    const comment = Comment.create({
+        content,
+        owner: req.user?._id,
+        video: videoId
+    })
+
+    if (!comment) {
+        throw new ApiError(500, "Comment could not be created")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(201, comment, "Comment Created Successfully")
+    )
 })
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
+    const {commentId} = req.params
+    const {content} = req.body
+
+    if (!content) {
+        throw new ApiError(400, "Please Enter the comment")
+    }
+
+    const comment = await Comment.findById(commentId)
+    if(!comment) {
+        throw new ApiError(404, "Comment not found")
+    }
+
+    if (comment.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "Not authorized to modify this comment")
+    }
+
+    comment.content = content
+    await comment.save()
+
+    return res.status(200).json(new ApiResponse(
+        200, comment, "Comment updated Successfully"
+    ))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -10,31 +10,167 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
+    const filter = {}
+    if (query) {
+        filter.$or = [
+            {title: {$regex: query, $options: "i"}},
+            {description: {$regex: query, $options: "i"}}
+        ]
+    }
+
+    if (userId) {
+        filter.owner = userId
+    }
+
+    const sort = {}
+    if (sortBy) {
+        sort[sortBy] = sortType === "desc" ? -1 : 1
+    } else {
+        // Default sorting by createdAt in desc order
+        sort.createdAt = -1
+    }
+
+    try {
+        const videos = await Video.aggregatePaginate([], {
+            // Using aggregatePaginate for pagination
+            page: parseInt(page),
+            limit: parseInt(limit),
+            sort: sort,
+            customLabels: {docs: "videos"}
+        })
+
+        return res.status(200).json(new ApiResponse(200, videos, "Success"))
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    const videoLocalPath = req.files?.video?.[0]?.path
+
+    let thumbnailLocalPath;
+    if (req.files && Array.isArray(req.files.thumbnail) && req.files.thumbnail.length > 0) {
+        thumbnailLocalPath = req.files.thumbnail[0].path
+    }
+
+    if (!videoLocalPath) {
+        throw new ApiError(400, "Video file is required")
+    }
+
+    const videoFile = await uploadOnCloudinary(videoLocalPath)
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+
+    const video = await Video.create({
+        title,
+        description,
+        videoFile: videoFile.url,
+        thumbnail: thumbnail.url
+    })
+
+    if (!video) {
+        throw new ApiError(400, "Failed to publish the video")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, video, "Published the video Successfully")
+    )
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+    if (!videoId) {
+        throw new ApiError(400, "Video Does not Exist")
+    }
+    const video = await Video.findById(videoId)
+    if (!video) {
+        throw new ApiError(400, "Failed to fetch the video ")
+    }
+    return res.status(200).json(
+        new ApiResponse(200, video, "Successfully fetched the video")
+    )
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+    if (!videoId) {
+        throw new ApiError(400, "Video Does not Exist")
+    }
 
+    const {title, description} = req.body
+    if (!(title || description)) {
+        throw new ApiError(400, "Please update the details")
+    }
+
+    let avatar = null //in case no avatar is provided
+    if (req.file) { 
+        const uploadResult = await uploadOnCloudinary(req.file?.path)
+        if (!uploadResult) {
+            throw new ApiError(500, 'Error uploading to Cloudinary')
+        }
+
+        avatar = uploadResult.url
+    }    
+
+    const video = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $set: {
+                title,
+                description,
+                avatar
+            }
+        },
+        {new: true}
+    )
+
+    if (!video) {
+        throw new ApiError(400, "Failed to update the video")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, video, "Video Updated Successfully")
+    )
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: delete video
+    if (!videoId) {
+        throw new ApiError(400, "Video Does not exists")
+    }
+    // console log this deleted video shit
+    const deletedVideo = await Video.findByIdAndDelete(videoId)
+
+    if (!deletedVideo) {
+        throw new ApiError(400, "Failed to delete the video")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, {}, "Video Deleted Successfully")
+    )
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+
+    if (!videoId) {
+        throw new ApiError(400, "Video does not exists")
+    }
+
+    const video = await Video.findById(videoId)
+
+    video.isPublished = !video.isPublished
+
+    await video.save()
+
+    return res.status(200).json(
+        new ApiResponse(200, video, "Publish status toggled successfully")
+    )
 })
 
 export {
