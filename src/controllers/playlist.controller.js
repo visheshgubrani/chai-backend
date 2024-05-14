0ad00a8040ad494e03d8c55b51e010cd551f1e13
Vishesh <vishesh.gubrani@gmail.com>
2024-05-14 19:04:37 +0530

    Playlist controllers completed

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,53 +1,156 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Playlist} from "../models/playlist.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { Playlist } from "../models/playlist.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
-    const {name, description} = req.body
+  const { name, description } = req.body;
 
-    //TODO: create playlist
-})
+  const user = req.user._id;
+
+  if (!name) {
+    throw new ApiError(400, "Please Enter the Name of Playlist");
+  }
+
+  const playlist = await Playlist.create({
+    name,
+    description,
+    owner: user,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, playlist, "playlist created successfully"));
+});
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-    const {userId} = req.params
-    //TODO: get user playlists
-})
+  const { userId } = req.params;
+  //TODO: get user playlists
+  const userPlaylist = await Playlist.find({
+    owner: userId,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, userPlaylist, "Playlists Fetched successfully"));
+});
 
 const getPlaylistById = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    //TODO: get playlist by id
-})
+  const { playlistId } = req.params;
+  //TODO: get playlist by id
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(400, "Failed to fetch the playlist");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "Playlist fetched successfully"));
+});
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-})
+  const { playlistId, videoId } = req.params;
+  //  jiska account ho ussi ki id ki playlist dikhaye
+  const user = req.user._id;
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(400, "Playlist not found");
+  }
+  if (user.toString() !== playlist.owner.toString()) {
+    throw new ApiError(400, "Not authorised to add videos");
+  }
+
+  const video = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $push: {
+        videos: videoId,
+      },
+    },
+    { new: true }
+  );
+  res.status(200).json(new ApiResponse(200, {}, "Added video successfully"));
+});
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-    // TODO: remove video from playlist
+  const { playlistId, videoId } = req.params;
+  // TODO: remove video from playlist
+  const user = req.user._id;
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(400, "Playlist not found");
+  }
 
-})
+  if (user.toString() !== playlist.owner.toString()) {
+    throw new ApiError(400, "User not authorized");
+  }
+
+  await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $pull: {
+        video: videoId,
+      },
+    },
+    { new: true }
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Video removed successfully"));
+});
 
 const deletePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    // TODO: delete playlist
-})
+  const { playlistId } = req.params;
+  // TODO: delete playlist
+  const user = req.user._id;
+  const playlist = await Playlist.findById(playlistId);
+  if (user.toString() !== playlist.owner.toString()) {
+    throw new ApiError(400, "User not authorized");
+  }
+
+  await Playlist.findByIdAndDelete(playlistId);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Playlist deleted successfully"));
+});
 
 const updatePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    const {name, description} = req.body
-    //TODO: update playlist
-})
+  const { playlistId } = req.params;
+  const { name, description } = req.body;
+
+  if (!(name || description)) {
+    throw new ApiError(400, "Please Enter the details");
+  }
+  const user = req.user._id;
+
+  const playlist = await Playlist.findById(playlistId);
+  if (user.toString() !== playlist.owner.toString()) {
+    throw new ApiError(400, "User not authorized");
+  }
+
+  await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      name,
+      description,
+    },
+    {
+      new: true,
+    }
+  );
+
+  return res.status(200).json(new ApiResponse(200, {}, "Playlist Updated"));
+});
 
 export {
-    createPlaylist,
-    getUserPlaylists,
-    getPlaylistById,
-    addVideoToPlaylist,
-    removeVideoFromPlaylist,
-    deletePlaylist,
-    updatePlaylist
-}
+  createPlaylist,
+  getUserPlaylists,
+  getPlaylistById,
+  addVideoToPlaylist,
+  removeVideoFromPlaylist,
+  deletePlaylist,
+  updatePlaylist,
+};
--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,28 +1,118 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {User} from "../models/user.model.js"
-import { Subscription } from "../models/subscription.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    // TODO: toggle subscription
-})
+  const { channelId } = req.params;
+  // TODO: toggle subscription
+  const user = req.user._id;
+  if (!channelId) {
+    throw new ApiError(400, "Failed to fetch the channel");
+  }
+
+  const subscribe = await Subscription.findOne({
+    subscriber: user,
+    channel: channelId,
+  });
+
+  if (subscribe) {
+    // Unsubscribe
+    await Subscription.findByIdAndDelete(subscribe?._id);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Unsunscribed Successfully"));
+  }
+
+  await Subscription.create({
+    subscriber: user,
+    channel: channelId,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Subscribed Successfully"));
+});
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-})
+  const { channelId } = req.params;
+
+  if (!mongoose.isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid Channel Id");
+  }
+
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: {
+        subscriber: new mongoose.Types.ObjectId(channelId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "channel",
+        foreignField: "_id",
+        as: "subscribers",
+      },
+    },
+    {
+      $unwind: "$subscribers",
+    },
+    {
+      $project: {
+        _id: "$subscribers._id",
+        username: "$subscribers.username",
+        avatar: "$subscibers.avatar",
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, subscribers, "Subscribers fetched successfully")
+    );
+});
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
-})
+  const { subscriberId } = req.params;
 
-export {
-    toggleSubscription,
-    getUserChannelSubscribers,
-    getSubscribedChannels
-}
+  if (!mongoose.isValidObjectId(subscriberId)) {
+    throw new ApiError(400, "Invalid Subscriber Id");
+  }
+
+  const subscribed = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(subscriberId),
+      },
+    },
+    {
+      $lookup: {
+        _id: "users",
+        localField: "subscriber",
+        foreignField: "_id",
+        as: "subscribed",
+      },
+    },
+    {
+      $unwind: "$subscribed",
+    },
+    {
+      $project: {
+        _id: "$subscribed._id",
+        username: "$subscribed.username",
+        avatar: "$subscribed.avatar",
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, subscribed, "Channels fetched successfully"));
+});
+
+export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
